c536bc3beb719ce164159e1264ab30ca56df90a0
Azamat H. Hackimov <azamat.hackimov@gmail.com>
2016-09-27 20:20:14 +0500

    Some improvements
    Added initial support for translated files.
    Cleanup and fixes.

--- RimTranslate.py
+++ RimTranslate.py
@@ -22,24 +22,30 @@ labels = [
     'beginLetterLabel',
     'description',
     'fixedName',
+    'gerund',
     'helpText',
     'inspectLine',
     'label',
     'letterLabel',
     'letterText',
+    'pawnLabel',
     'pawnsPlural',
+    'rulesStrings',         # hard one
     'recoveryMessage',
     'reportString',
+    'skillLabel',
     'text',
-];
+    'verb',
+]
 
 defNames = [
     'defName',
     'DefName',  # Some DefNames with first uppercase letter
 ]
 
-def create_pot_file (filename):
-    "Create POT file (only source strings exists) from given filename"
+
+def create_pot_file(filename):
+    """Create POT file (only source strings exists) from given filename"""
     doc = etree.parse(filename)
     po = polib.POFile()
     [undef, basefile] = filename.split(args.source_dir, 1)
@@ -69,7 +75,7 @@ def create_pot_file (filename):
                         print("  Found Label '%s' (%s)" % (label, doc.getpath(label_node)))
 
                         # Generate string for parenting
-                        [undef, path_label] = doc.getpath(label_node).split(doc.getpath(parent) ,1)
+                        [undef, path_label] = doc.getpath(label_node).split(doc.getpath(parent), 1)
                         path_label = re.sub(r'\/', '.', path_label)
                         path_label = re.sub(r'\.li\.', '.0.', path_label)
                         match = re.search(r'\.li\[(\d+)\]\.', path_label)
@@ -78,19 +84,35 @@ def create_pot_file (filename):
                         print("  msgctxt: " + defName_node.text + path_label)
 
                         entry = polib.POEntry(
-                            msgctxt = defName_node.text + path_label,
-                            msgid = label_node.text,
+                            msgctxt=defName_node.text + path_label,
+                            msgid=label_node.text,
                             occurrences=[(basefile, str(label_node.sourceline))]
                         )
                         po.append(entry)
     # sort by line in source file
-    po.sort(key = lambda x: int(x.occurrences[0][1]))
+    po.sort(key=lambda x: int(x.occurrences[0][1]))
 
     return po
 
+
+def create_translation_file(po_file):
+    languagedata = etree.Element('LanguageData')
+    xml = etree.ElementTree(languagedata)
+    po = polib.pofile(po_file)
+    for po_entry in po:
+        if (po_entry.msgstr != "") and ('fuzzy' not in po_entry.flags):
+            print(po_entry.msgctxt)
+            entry = etree.SubElement(languagedata, po_entry.msgctxt)
+            entry.text = str(po_entry.msgstr)
+    outfile = open('out.xml', 'w')
+    # Hack - silly lxml cannot write native unicode strings
+    outfile.write(etree.tostring(xml, pretty_print=True, xml_declaration=True, encoding='utf-8').decode('utf-8'))
+    return 0
+
+
 for root, dirs, files in os.walk(args.source_dir):
     for dir in dirs:
-        full_filename = os.path.join(root,dir)
+        full_filename = os.path.join(root, dir)
         [udnef, filedir] = full_filename.split(args.source_dir, 1)
         # Create po directory structure
         po_full_filename = os.path.join(args.po_dir, filedir)
@@ -112,12 +134,12 @@ for root, dirs, files in os.walk(args.source_dir):
             pofilename = os.path.join(args.po_dir, filedir)
 
             if os.path.exists(pofilename + ".po"):
+                print("  Updating PO file " + pofilename + ".po")
                 po = polib.pofile(pofilename + ".po")
                 po.merge(pot)
-                po.sort(key = lambda x: int(x.occurrences[0][1]))
-                print("  Updating PO file " + pofilename + ".po")
                 po.save(pofilename + ".po")
             else:
-                pot.save(pofilename + ".po")
                 print("  Creating PO file " + pofilename + ".po")
+                pot.save(pofilename + ".po")
 
+create_translation_file('po/SkillDefs/Skills.xml.po')
