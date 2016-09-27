99a4ceae480b844833794be910c2b7a133d1a7f7
Azamat H. Hackimov <azamat.hackimov@gmail.com>
2016-09-27 10:39:17 +0500

    Initial commit

---
+++ RimTranslate.py
@@ -0,0 +1,123 @@
+#!/usr/bin/python
+
+import os
+import re
+from lxml import etree
+import polib
+import argparse
+import datetime
+
+parser = argparse.ArgumentParser(description='Creating Gettext PO files and DefInjections for RimWorld translations.')
+parser.add_argument('--source-dir', '-s',   action='store', type=str, required=True, help='Root source dir where all Defs (ex. ~/.local/share/Steam/SteamApps/common/RimWorld/Mods/Core/Defs/)')
+parser.add_argument('--po-dir',     '-p',   action='store', type=str, required=True, help='Dir where will be placed PO files')
+parser.add_argument('--output-dir', '-o',   action='store', type=str, required=True, help='Output dir')
+
+args = parser.parse_args()
+
+# Add trailing slash for sure
+args.source_dir = os.path.join(args.source_dir, '')
+
+labels = [
+    'beginLetter',
+    'beginLetterLabel',
+    'description',
+    'fixedName',
+    'helpText',
+    'inspectLine',
+    'label',
+    'letterLabel',
+    'letterText',
+    'pawnsPlural',
+    'recoveryMessage',
+    'reportString',
+    'text',
+];
+
+defNames = [
+    'defName',
+    'DefName',  # Some DefNames with first uppercase letter
+]
+
+def create_pot_file (filename):
+    "Create POT file (only source strings exists) from given filename"
+    doc = etree.parse(filename)
+    po = polib.POFile()
+    [undef, basefile] = filename.split(args.source_dir, 1)
+    po.metadata = {
+        'Project-Id-Version': '1.0',
+        'Report-Msgid-Bugs-To': 'you@example.com',
+        'POT-Creation-Date': str(datetime.datetime.now().strftime("%Y-%m-%d %H:%M")),
+        'PO-Revision-Date': str(datetime.datetime.now().strftime("%Y-%m-%d %H:%M")),
+        'Last-Translator': 'Some Translator <yourname@example.com>',
+        'Language-Team': 'English <yourteam@example.com>',
+        'MIME-Version': '1.0',
+        'Content-Type': 'text/plain; charset=utf-8',
+        'Content-Transfer-Encoding': '8bit',
+    }
+    po.metadata_is_fuzzy = 1
+
+    for defName in defNames:
+        for defName_node in doc.findall("//" + defName):
+            if defName_node is not None:
+                parent = defName_node.getparent()
+                print("  Found defName '%s' (%s)" % (defName_node.text, doc.getpath(parent)))
+                for label in labels:
+                    parent = defName_node.getparent()
+                    print ("  Checking label %s" % (label))
+                    label_nodes = parent.findall(".//" + label)
+                    for label_node in label_nodes:
+                        print("  Found Label '%s' (%s)" % (label, doc.getpath(label_node)))
+
+                        # Generate string for parenting
+                        [undef, path_label] = doc.getpath(label_node).split(doc.getpath(parent) ,1)
+                        path_label = re.sub(r'\/', '.', path_label)
+                        path_label = re.sub(r'\.li\.', '.0.', path_label)
+                        match = re.search(r'\.li\[(\d+)\]\.', path_label)
+                        if match:
+                            path_label = re.sub(r'\.li\[\d+\]\.', "." + str(int(match.group(1)) - 1) + ".", path_label)
+                        print("  msgctxt: " + defName_node.text + path_label)
+
+                        entry = polib.POEntry(
+                            msgctxt = defName_node.text + path_label,
+                            msgid = label_node.text,
+                            occurrences=[(basefile, str(label_node.sourceline))]
+                        )
+                        po.append(entry)
+    # sort by line in source file
+    po.sort(key = lambda x: int(x.occurrences[0][1]))
+
+    return po
+
+for root, dirs, files in os.walk(args.source_dir):
+    for dir in dirs:
+        full_filename = os.path.join(root,dir)
+        [udnef, filedir] = full_filename.split(args.source_dir, 1)
+        # Create po directory structure
+        po_full_filename = os.path.join(args.po_dir, filedir)
+        if not(os.path.exists(po_full_filename)):
+            os.makedirs(po_full_filename)
+
+        # Create InjDefs directory structure
+        output_full_filename = os.path.join(args.output_dir, filedir)
+        if not(os.path.exists(output_full_filename)):
+            os.makedirs(output_full_filename)
+
+    for file in files:
+        if file.endswith('.xml'):
+            full_filename = os.path.join(root,file)
+            print("Processing " + full_filename)
+
+            [undef, filedir] = full_filename.split(args.source_dir, 1)
+            pot = create_pot_file(full_filename)
+            pofilename = os.path.join(args.po_dir, filedir)
+
+            if os.path.exists(pofilename + ".po"):
+                po = polib.pofile(pofilename + ".po")
+                po.merge(pot)
+                po.sort(key = lambda x: int(x.occurrences[0][1]))
+                print("  Updating PO file " + pofilename + ".po")
+                po.save(pofilename + ".po")
+            else:
+                pot.save(pofilename + ".po")
+                print("  Creating PO file " + pofilename + ".po")
+
