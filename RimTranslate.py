a43c99d7820c5c09d4efd5dac211aedf1ec5c524
Azamat H. Hackimov <azamat.hackimov@gmail.com>
2016-09-27 22:34:49 +0500

    Added logging facility and reworked argparse logic.

--- RimTranslate.py
+++ RimTranslate.py
@@ -6,13 +6,36 @@ from lxml import etree
 import polib
 import argparse
 import datetime
+import logging
 
-parser = argparse.ArgumentParser(description='Creating Gettext PO files and DefInjections for RimWorld translations.')
-parser.add_argument('--source-dir', '-s',   action='store', type=str, required=True, help='Root source dir where all Defs (ex. ~/.local/share/Steam/SteamApps/common/RimWorld/Mods/Core/Defs/)')
-parser.add_argument('--po-dir',     '-p',   action='store', type=str, required=True, help='Dir where will be placed PO files')
-parser.add_argument('--output-dir', '-o',   action='store', type=str, required=True, help='Output dir')
+parser = argparse.ArgumentParser(description='Creating Gettext PO files and DefInjections for RimWorld translations.',
+                                 formatter_class=argparse.RawTextHelpFormatter)
+
+group_source = parser.add_argument_group('Extracting options')
+group_source.add_argument('--source-dir', '-s', type=str,
+                          help='''Root source dir where all Defs (ex. ~/.local/share/Steam/SteamApps/common/RimWorld/Mods/Core/Defs/)''')
+
+group_generation = parser.add_argument_group('Generation options')
+group_generation.add_argument('--output-dir', '-o', type=str,
+                              help='Directory where will be placed InjDefs XML-files with actual translations')
+
+parser.add_argument('--po-dir', '-p', type=str,
+                    help='Directory where will be placed generated or updated PO-files')
+
+parser.add_argument('-v', type=str, default='CRITICAL',
+                    help='Enable verbose output (debug, info, warning, error, critical)')
+
 
 args = parser.parse_args()
+
+if not ((args.output_dir or args.source_dir) and args.po_dir):
+    parser.error('''No action requested. The following arguments are required:
+  "--source-dir <SOURCE_DIR> --po-dir <PO_DIR>"
+  or
+  "--output-dir <SOURCE_DIR> --po-dir <PO_DIR>"''')
+
+log_level = getattr(logging, str.upper(args.v))
+logging.basicConfig(format='%(levelname)s: %(message)s', level=log_level)
 
 # Add trailing slash for sure
 args.source_dir = os.path.join(args.source_dir, '')
@@ -42,6 +65,7 @@ defNames = [
     'defName',
     'DefName',  # Some DefNames with first uppercase letter
 ]
+
 
 def generate_injdef_xml_tag(string):
     """Create XML tag for InjectDefs"""
@@ -76,19 +100,19 @@ def create_pot_file(filename):
         for defName_node in doc.findall("//" + defName):
             if defName_node is not None:
                 parent = defName_node.getparent()
-                print("  Found defName '%s' (%s)" % (defName_node.text, doc.getpath(parent)))
+                logging.debug("Found defName '%s' (%s)" % (defName_node.text, doc.getpath(parent)))
                 for label in labels:
                     parent = defName_node.getparent()
-                    print("  Checking label %s" % label)
+                    logging.debug("Checking label %s" % label)
                     label_nodes = parent.findall(".//" + label)
                     for label_node in label_nodes:
-                        print("  Found Label '%s' (%s)" % (label, doc.getpath(label_node)))
+                        logging.debug("Found Label '%s' (%s)" % (label, doc.getpath(label_node)))
                         if len(label_node):
-                            print(" Element has children")
+                            logging.debug("Element has children")
                             for child_node in label_node:
                                 [undef, path_label] = doc.getpath(child_node).split(doc.getpath(parent), 1)
                                 path_label = generate_injdef_xml_tag(path_label)
-                                print("  msgctxt: " + defName_node.text + path_label)
+                                logging.debug("msgctxt: " + defName_node.text + path_label)
 
                                 entry = polib.POEntry(
                                     msgctxt=defName_node.text + path_label,
@@ -100,7 +124,7 @@ def create_pot_file(filename):
                             # Generate string for parenting
                             [undef, path_label] = doc.getpath(label_node).split(doc.getpath(parent), 1)
                             path_label = generate_injdef_xml_tag(path_label)
-                            print("  msgctxt: " + defName_node.text + path_label)
+                            logging.debug("msgctxt: " + defName_node.text + path_label)
 
                             entry = polib.POEntry(
                                 msgctxt=defName_node.text + path_label,
@@ -120,7 +144,6 @@ def create_translation_file(po_file):
     po = polib.pofile(po_file)
     for po_entry in po:
         if (po_entry.msgstr != "") and ('fuzzy' not in po_entry.flags):
-            print(po_entry.msgctxt)
             entry = etree.SubElement(languagedata, po_entry.msgctxt)
             entry.text = str(po_entry.msgstr)
     outfile = open('out.xml', 'w')
@@ -132,7 +155,7 @@ def create_translation_file(po_file):
 for root, dirs, files in os.walk(args.source_dir):
     for dir in dirs:
         full_filename = os.path.join(root, dir)
-        [udnef, filedir] = full_filename.split(args.source_dir, 1)
+        [undef, filedir] = full_filename.split(args.source_dir, 1)
         # Create po directory structure
         po_full_filename = os.path.join(args.po_dir, filedir)
         if not(os.path.exists(po_full_filename)):
@@ -146,19 +169,19 @@ for root, dirs, files in os.walk(args.source_dir):
     for file in files:
         if file.endswith('.xml'):
             full_filename = os.path.join(root, file)
-            print("Processing " + full_filename)
+            logging.info("Processing " + full_filename)
 
             [undef, filedir] = full_filename.split(args.source_dir, 1)
             pot = create_pot_file(full_filename)
             pofilename = os.path.join(args.po_dir, filedir)
 
             if os.path.exists(pofilename + ".po"):
-                print("  Updating PO file " + pofilename + ".po")
+                logging.info("Updating PO file " + pofilename + ".po")
                 po = polib.pofile(pofilename + ".po")
                 po.merge(pot)
                 po.save(pofilename + ".po")
             else:
-                print("  Creating PO file " + pofilename + ".po")
+                logging.info("Creating PO file " + pofilename + ".po")
                 pot.save(pofilename + ".po")
 
 create_translation_file('po/SkillDefs/Skills.xml.po')
