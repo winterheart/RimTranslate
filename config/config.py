d3f2891ff94bdf0885228e9b0b21d7ee623ca2ee
Mohamed Moustafa <hi@fairewinds.ca>
2024-05-02 10:58:19 +0200

    Remove unnecessary TODOs

--- config/config.py
+++ config/config.py
@@ -1,14 +1,13 @@
 pot_metadata = {
-    # TODO: this should be set per-translator
     "language_team": {
-        "name": "English",
-        "email": "yourteam@example.com"
+        "name": "Ludeon/translators-arabic (https://github.com/orgs/Ludeon/teams/translators-arabic)",
+        "email": "hi@fairewinds.ca"
     },
     "last_translator": {
-        "name": "Some Translator",
-        "email": "yourname@example.com"
+        "name": "Mohamed Moustafa",
+        "email": "hi@fairewinds.ca"
     },
-    "report_msgid_bugs_to": "you@example.com"
+    "report_msgid_bugs_to": "hi@fairewinds.ca"
 }
 
 version = "0.7.0"
